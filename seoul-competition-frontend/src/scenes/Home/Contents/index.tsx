7c4e4ec4d5382e68590d852d5bbdf7950226c958
Kim Yoo <kimyoo04eco@naver.com>
2023-05-22 22:45:13 +0900

    css: 홈페이지 contents 컴포넌트 w-full 추가

--- seoul-competition-frontend/src/scenes/Home/Contents/index.tsx
+++ seoul-competition-frontend/src/scenes/Home/Contents/index.tsx
@@ -3,7 +3,7 @@ import ContentItem from "../ContentItem";
 
 export default function Contents() {
   return (
-    <div className="col-center gap-10">
+    <div className="col-center w-full gap-10">
       <h2 className="text-2xl font-bold">오늘은 무엇을 하고 싶으신가요?</h2>
 
       <div className="col-center w-full max-w-7xl gap-4 md:flex-row md:justify-between md:gap-8 lg:gap-12">
