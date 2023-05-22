c1c6d4d5080e3a5e4f1572ec6738c9430f819bf9
Kim Yoo <kimyoo04eco@naver.com>
2023-05-23 01:45:04 +0900

    fix: 교육 정보 디테일 페이지 table 태그 구현

--- seoul-competition-frontend/src/scenes/Educations/EducationDetail/EducationDetailItem/content.tsx
+++ seoul-competition-frontend/src/scenes/Educations/EducationDetail/EducationDetailItem/content.tsx
@@ -10,48 +10,52 @@ export default function Content({ data }: IContentProps) {
   return (
     <>
       {/* 교육 정보 영역 */}
-      <div>
-        {/* 내용 영역*/}
+      <div className="col-center w-full gap-8 py-8">
+        <table className="education-detail__table">
+          <tr>
+            <td>정원</td>
+            <td>{data.capacity} 명</td>
+          </tr>
+          <tr>
+            <td>가격</td>
+            <td>{data.price === "0" ? "무료" : `${data.price} 원`}</td>
+          </tr>
+          <tr>
+            <td>신청 기간</td>
+            <td>
+              <div>
+                <span>{getKorDate(data.registerStart)}</span>
+                <span> ~ </span>
+                <span>{getKorDate(data.registerEnd)}</span>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>교육 기간</td>
+            <td>
+              <div>
+                <span>{getKorDate(data.educationStart)}</span>
+                <span> ~ </span>
+                <span>{getKorDate(data.educationEnd)}</span>
+              </div>
+            </td>
+          </tr>
+        </table>
 
-        {/* 작성자, 작성일 */}
-        <div className="col-start mb-8 text-sm text-gray_2">
-          <div>
-            <span>{data.status}</span>
-
-            {/* 외부 링크 이동 버튼 */}
-            <a
-              href={data.url}
-              rel="noopener"
-              target="_blank"
-              className="rounded-full bg-main_color px-2 py-1 text-font_white"
-            >
-              <button>수강 신청 페이지</button>
-            </a>
-          </div>
-
-          <div>
-            <h4 className="text-lg">정원</h4>
-            <span>{data.capacity}</span>
-          </div>
-
-          <div>
-            <h4 className="text-lg">가격</h4>
-            <span>{data.price === "0" ? "무료" : `${data.price} 원`}</span>
-          </div>
-
-          <h4 className="text-lg">신청 기간</h4>
-          <div>
-            <span>{getKorDate(data.registerStart)}</span>
-            <span> ~ </span>
-            <span>{getKorDate(data.registerEnd)}</span>
-          </div>
-
-          <h4 className="text-lg">교육 기간</h4>
-          <div>
-            <span>{getKorDate(data.educationStart)}</span>
-            <span> ~ </span>
-            <span>{getKorDate(data.educationEnd)}</span>
-          </div>
+        {/* 문구와 외부 링크 버튼 */}
+        <div className="flex w-full items-center justify-between gap-6">
+          <span>
+            버튼을 눌러 더 많은 정보를 확인하고 수강신청을 해보세요. 당신의 인생
+            2모작을 응원합니다.
+          </span>
+          <a
+            href={data.url}
+            rel="noopener"
+            target="_blank"
+            className="col-center h-8 rounded-full bg-main_color text-font_white"
+          >
+            <button className="w-36">수강 신청 페이지</button>
+          </a>
         </div>
       </div>
     </>
