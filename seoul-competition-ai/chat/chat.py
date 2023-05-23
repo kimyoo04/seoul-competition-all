f48d5a9cbe206eb803d04958070ba8b77cd39ff8
팡다츄 <fox3931@gmail.com>
2023-05-23 20:40:59 +0900

    fix: 챗봇데이터에서 답변이 안되는 경우 GPT를 통한 답변

--- seoul-competition-ai/chat/chat.py
+++ seoul-competition-ai/chat/chat.py
@@ -2,12 +2,29 @@ from sklearn.metrics.pairwise import cosine_similarity
 from transformers import AutoTokenizer, AutoModel
 
 import torch
-from sklearn.feature_extraction.text import TfidfVectorizer
-
 import numpy as np
 import pandas as pd
 import os
 import joblib
+import openai
+
+guide_msg = '''
+                1. 사이트 종류: 교육 정보 공유 사이트
+                2. 기능:
+                    교육 정보 게시판: 교육과 관련된 정보를 게시하고 공유하는 공간입니다. 교육정보 페이지 우측 하단에 '골라보기' 버튼을 클릭하여 확인할 수 있습니다.
+                    자유게시판: 교육과는 관련없는 다양한 주제로 글을 작성하고 의견을 공유하는 공간입니다.
+                    댓글 기능: 교육 정보나 게시글에 대한 댓글을 작성하고 수정, 삭제할 수 있습니다. 댓글 작성 시 비밀번호를 설정하여 식별이 가능합니다.
+                    별점: 교육 정보에 대한 별점을 남기는 기능은 제공하지 않습니다.
+                    검색 기능: 교육 정보 검색 기능은 제공합니다. 사용자가 검색한 교육 정보에 대하여 유사한 교육 정보를 추천해줍니다.
+                    사용자 식별: 댓글 작성 시 비밀번호를 설정하여 수정과 삭제를 제한적으로 가능하게 합니다.
+                
+                사용자가 인사 및 고마움의 표현에 대해서는 자연스럽게 답변할 것.
+                조건에 맞지 않는 질문의 경우에는 최대한 친절하게 답할 것.
+
+                답변은 최대한 한 문장으로 끝낼것.
+
+                질문 :
+            '''
 
 def check_chat_data() :
     '''
@@ -94,6 +111,19 @@ def mean_pooling(model_output, attention_mask):
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
 
+def chatGPT(prompt) :
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+
+        messages=[
+        {"role": "user", "content": prompt},
+        ]
+        ,
+        temperature = 0.3
+    )
+    return response
+
+
 def use_chatbot(user_question, chatbot_model) :
     '''
     - 사용자 입력 문자열에 대하여 챗봇의 답변을 문자열로 반환
@@ -104,8 +134,9 @@ def use_chatbot(user_question, chatbot_model) :
     '''
 
     if check_chat_data()==True :
-        #chatbot_model = load_chatbot_model()
-        chatbot_data  = load_chatbot_data()
+        origin_chatbot_data  = load_chatbot_data()
+        chatbot_data = origin_chatbot_data.copy()
+
         chatbot_tokenizer = load_chatbot_tokenizer()
 
         encoded_input = chatbot_tokenizer(user_question, padding=True, truncation=True, return_tensors='pt')  
@@ -119,10 +150,38 @@ def use_chatbot(user_question, chatbot_model) :
 
         chatbot_data["cosin"] = chatbot_data["embedding"].map(lambda x : cosine_similarity([final_input_data],[x]).squeeze())
 
-        # 해당 수치는 조정해보는 것도 좋음.
-        # if chatbot_data["cosin"].max() < 0.50 :
-        if chatbot_data["cosin"].max() < 0.50 :
-            answer = "핵심 키워드나 다른 키워드를 사용하여 다시 질문 해주시겠어요 ?"
+
+        if chatbot_data["cosin"].max() < 0.60 :
+            # GPT 호출 
+            OPENAI_API_KEY = os.environ.get("FASTAPI_OPENAI_KEY")
+            openai.api_key = OPENAI_API_KEY
+
+            # 사전 제약조건 + 유저의 입력
+            prompt = guide_msg + user_question
+
+            # 간혹 GPT 응답 오류 처리 
+            try : 
+                response = chatGPT( prompt )
+            
+            except : 
+                answer = "잠시 문제가 발생했습니다. 다시 입력해주세요."
+                return answer 
+
+            answer = response["choices"][0]["message"]["content"]
+
+            temp_dict = {"question" : user_question, 
+                         "answer" : answer, 
+                         "embedding" : [final_input_data] }
+            
+            temp_df = pd.DataFrame(temp_dict)
+
+            # GPT에서 생성된 대화를 챗봇 데이터에 추가 
+            origin_chatbot_data = pd.concat([origin_chatbot_data, temp_df], ignore_index=True)
+
+            path = os.path.join(os.getcwd(), 'data', 'chatbot_data_test.pkl')
+            origin_chatbot_data.to_pickle(path)
+
+            # 답변 반환
             return answer 
 
     else :
