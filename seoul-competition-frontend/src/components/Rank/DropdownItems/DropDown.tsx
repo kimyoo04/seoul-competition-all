448f50ae8e84d0d28860a794306295a917adeb0b
Kim Yoo <kimyoo04eco@naver.com>
2023-05-27 12:20:17 +0900

    css: DropDown 컴포넌트 h-6 -> h-8 수정

--- seoul-competition-frontend/src/components/Rank/DropdownItems/DropDown.tsx
+++ seoul-competition-frontend/src/components/Rank/DropdownItems/DropDown.tsx
@@ -1,7 +1,7 @@
 export default function DropDown({ text = "" }: { text?: string }) {
   return (
     <div className="col-center">
-      <button className="flex h-6 w-52 items-center justify-between rounded-t-md bg-main_color/90  shadow-md">
+      <button className="flex h-8 w-52 cursor-default items-center justify-between rounded-t-md bg-main_color/90  shadow-md">
         <div className="h-9 w-9"></div>
         <span className="text-font_white">{text}</span>
         <div className="h-9 w-9"></div>
