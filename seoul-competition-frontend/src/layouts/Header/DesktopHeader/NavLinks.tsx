5e170080bd3afb6e9e99d7b400e5dd2b8cd35cb1
Kim <58503130+kimyoo04@users.noreply.github.com>
2023-05-27 17:33:34 +0900

    fix: 뒤로가기, 목록 버튼 클릭 시, 기존 검색어 유지하도록 Link 태그 안에 forwardRef 컴포넌트를 사용

--- seoul-competition-frontend/src/layouts/Header/DesktopHeader/NavLinks.tsx
+++ seoul-competition-frontend/src/layouts/Header/DesktopHeader/NavLinks.tsx
@@ -1,35 +1,54 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
 import ButtonWrapper from "@components/Animation/ButtonWrapper";
+import { ForwardedRef, forwardRef } from "react";
+import { useAppDispatch } from "@toolkit/hook";
+import { searchActions } from "@features/search/searchSlice";
+
+interface ButtonProps {
+  onClick: () => void;
+  href: string;
+  text: string;
+}
+
+const CustomLink = forwardRef<HTMLAnchorElement, ButtonProps>(
+  function CustomLink(
+    { onClick, href, text },
+    ref: ForwardedRef<HTMLAnchorElement>
+  ) {
+    const router = useRouter();
+    const pathsArr = router.asPath;
+
+    return (
+      <a href={href} onClick={onClick} ref={ref}>
+        <ButtonWrapper>
+          <span
+            className={`text-lg ${
+              pathsArr.includes(href) ? "font-bold text-main_color" : ""
+            }`}
+          >
+            {text}
+          </span>
+        </ButtonWrapper>
+      </a>
+    );
+  }
+);
 
 export default function NavLinks() {
-  const router = useRouter();
-  const pathsArr = router.asPath.split("/");
+  const dispatch = useAppDispatch();
+  const onClick = () => {
+    dispatch(searchActions.resetKeyword()); // 검색 키워드 초기화
+  };
 
   return (
     <div className="flex gap-2 text-xl font-medium  md:gap-4">
       {/* 링크들 */}
-      <Link href={"/educations"} className="">
-        <ButtonWrapper>
-          <span
-            className={`text-lg ${
-              pathsArr.includes("educations") ? "font-bold text-main_color" : ""
-            }`}
-          >
-            교육 정보
-          </span>
-        </ButtonWrapper>
+      <Link href={"/educations"} passHref legacyBehavior>
+        <CustomLink href={"/educations"} onClick={onClick} text={"교육 정보"} />
       </Link>
-      <Link href={"/posts"} className="">
-        <ButtonWrapper>
-          <span
-            className={`text-lg ${
-              pathsArr.includes("posts") ? "font-bold text-main_color" : ""
-            }`}
-          >
-            자유게시판
-          </span>
-        </ButtonWrapper>
+      <Link href={"/posts"} passHref legacyBehavior>
+        <CustomLink href={"/posts"} onClick={onClick} text={"자유게시판"} />
       </Link>
     </div>
   );
